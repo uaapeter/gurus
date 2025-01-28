a3a6bbdc354d694d6190078e17293dd0a0d2255a
uaapeter <uaapeter@gmail.com>
2025-01-28 11:06:10 +0000

    attendanceid

--- constant.tsx
+++ constant.tsx
@@ -1,4 +1,4 @@
-// export const DOMAIN = 'https://api.gurusnestmedspa.com'
-export const DOMAIN = `http://127.0.0.1:8000`
+export const DOMAIN = 'https://api.gurusnestmedspa.com'
+// export const DOMAIN = `http://127.0.0.1:8000`
 // export const DOMAIN = 'https://pos-backend-beta.vercel.app'
 
