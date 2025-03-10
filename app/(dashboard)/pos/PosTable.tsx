0eb929d1d4297cc1a0bdd078344d0f6114ad72d1
uaapeter <uaapeter@gmail.com>
2025-03-10 15:20:46 +0000

    paidamount

--- app/(dashboard)/pos/PosTable.tsx
+++ app/(dashboard)/pos/PosTable.tsx
@@ -93,6 +93,7 @@ function PosTable({ users, token, charges, discounts, pendingsales}: { users: an
                 ...data,
                 orderType: 'SALE',
                 orderId,
+
                 amountInWords: amtToWords(sumTotal(cart, 'words')),
                 amount: data?.discount > 0 ? sumTotal(cart, 'total') - (sumTotal(cart, 'total')/100) * data.discount + (sumTotal(cart, 'total')/100) * data?.charge : sumTotal(cart, 'total') + (sumTotal(cart, 'total')/100) * data?.charge ,
                 customer: {
@@ -101,7 +102,7 @@ function PosTable({ users, token, charges, discounts, pendingsales}: { users: an
                     customerEmail: data?.customerEmail
                 },
                 paymentMethod: data?.paymentMethod,
-                status:  sumTotal(cart, 'total') - (sumTotal(cart, 'total')/100) * data.discount + (sumTotal(cart, 'total')/100) * data?.charge  > parseInt(data.totalPaid) ? 'Open': 'Paid' ,
+                status: callover?.totalPaid ? sumTotal(cart, 'total') - (sumTotal(cart, 'total')/100) * data.discount + (sumTotal(cart, 'total')/100) * data?.charge  > parseInt(callover.totalPaid) + parseInt (data?.totalPaid) ? 'Open'  : 'Paid' :  sumTotal(cart, 'total') - (sumTotal(cart, 'total')/100) * data.discount + (sumTotal(cart, 'total')/100) * data?.charge  > parseInt(data.totalPaid) ? 'Open': 'Paid' ,
                 VALUES
             }
             handleValidation(body.VALUES)
