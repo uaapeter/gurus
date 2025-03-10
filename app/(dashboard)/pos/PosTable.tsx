338741bb75bc8aa4a248ab3427ff19d934407307
uaapeter <uaapeter@gmail.com>
2025-03-10 15:24:56 +0000

    totalPaid

--- app/(dashboard)/pos/PosTable.tsx
+++ app/(dashboard)/pos/PosTable.tsx
@@ -93,7 +93,7 @@ function PosTable({ users, token, charges, discounts, pendingsales}: { users: an
                 ...data,
                 orderType: 'SALE',
                 orderId,
-
+                totalPaid: callover?.totalPaid ? parseInt(callover?.totalPaid) + parseInt(data?.totalPaid) : data?.totalPaid,
                 amountInWords: amtToWords(sumTotal(cart, 'words')),
                 amount: data?.discount > 0 ? sumTotal(cart, 'total') - (sumTotal(cart, 'total')/100) * data.discount + (sumTotal(cart, 'total')/100) * data?.charge : sumTotal(cart, 'total') + (sumTotal(cart, 'total')/100) * data?.charge ,
                 customer: {
